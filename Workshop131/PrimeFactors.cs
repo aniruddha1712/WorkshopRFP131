c4ef58a09abe351dca37b971328fe87d43fe63ce
Aniruddha <aniruddha1712@gmail.com>
2022-05-09 09:05:19 +0530

    [Aniruddha]Added: Method to find N prime factors

---
+++ Workshop131/PrimeFactors.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop131
+{
+    class PrimeFactors
+    {
+        public static void PrimeFact()
+        {
+            Console.WriteLine("Enter any number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=2; number > 1; i++)
+            {
+                if (number % i==0)
+                {
+                    int temp = 0;
+                    while (number % i==0)
+                    {
+                        number /= i;
+                        temp++;
+                    }
+                    Console.WriteLine("{0} is the prime factor of {1} times: ",i,temp);
+                }
+            }
+        }
+    }
+}
---
+++ Workshop131/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the Workshop");
+            PrimeFactors.PrimeFact();
+            Console.ReadLine();
+        }
+    }
+}
